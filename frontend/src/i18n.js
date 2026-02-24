cc78a3240b368e4e82d59b93bc17379c250e2e4e
SaydirasulovFirdavs <saydirasulovfirdavs040507@gmail.com>
2026-02-24 13:47:02 +0500

    fix(frontend): rename translations to translation_v2.json to perfectly bypass edge cache

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -16,7 +16,7 @@ i18n
         },
 
         backend: {
-            loadPath: '/locales/{{lng}}/translation.json?v=' + Date.now(),
+            loadPath: '/locales/{{lng}}/translation_v2.json?v=' + Date.now(),
         },
 
         detection: {
